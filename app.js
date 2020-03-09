55b1c138874970373f22083d08d2077f4c0be040
AngelizaMedina <46779042+AngelizaMedina@users.noreply.github.com>
2020-03-09 17:22:31 +0100

    Added node module "cors" to be able to fetch data without a CORS issue

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var app = express();
 var router = require("./router");
 var { testConnection } = require("./config/database");
 var formidable = require("express-formidable");
+var cors = require("cors");
 
 testConnection();
 app.use(formidable());
+app.use(cors());
 app.use(router);
 
 module.exports = app;
